2b9750d925454d9575a85fb2ebd930045bec2848
Robin Ponçon <poncon.robin@gmail.com>
2020-12-01 17:15:58 +0100

    Ajout d'un State pour afficher le nombre de pokémons

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState } from "react";
+
+import Pokemon from "./models/pokemon";
+import {POKEMONS} from "./models/mock-pokemon";
 
 const App: FunctionComponent = () => {
-    const name: String = "React";
+    //const [name, setName] = useState<String>("React");
+    const [pokemons] = useState<Pokemon[]>(POKEMONS);
     return(
-        <h1>Bonjour, {name} !</h1>
+        <div>
+            <h1>Pokédex</h1>
+            <p>Il y a {pokemons.length} pokémons dans le Pokédex.</p>
+        </div>
     )
 }
 
