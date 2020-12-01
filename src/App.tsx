a6c6635991286b2302e0f6712fe9766db975fb72
Robin Ponçon <poncon.robin@gmail.com>
2020-12-01 15:55:48 +0100

    Installation environnement pour React

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React, { FunctionComponent } from "react";
+
+const App: FunctionComponent = () => {
+    const name: String = "React";
+    return(
+        <h1>Bonjour, {name} !</h1>
+    )
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+// @ts-ignore
+import React from "react";
+// @ts-ignore
+import ReactDOM from "react-dom";
+import App from "./App";
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById("root")
+);
