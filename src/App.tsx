a9906240d8dd8cefd7c5915f1a94c16312cc55f6
Robin Ponçon <poncon.robin@gmail.com>
2020-12-07 17:20:07 +0100

    Factorisation des format date et type, ajout page détail sur Pokemon et gestion des routes de l'application

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
 import React, { FunctionComponent, useState, useEffect } from "react";
 import PokemonList from "./pages/pokemon-list";
+import PokemonDetail from "./pages/pokemon-detail";
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import PageNotFound from "./pages/page-not-found";
 
 const App: FunctionComponent = () => {
 
     return(
-        <PokemonList/>
+        <Router>
+            <div>
+                {/* La barre de navigation commun à toutes les pages*/}
+                    <nav>
+                        <div className="nav-wrapper teal">
+                            <Link to="/" className="brand-logo center">Pokédex</Link>
+                        </div>
+                    </nav>
+                {/* Le système de gestion des routes de notre application */}
+                <Switch>
+                    <Route exact path="/" component={PokemonList}/>
+                    <Route exact path="/pokemons" component={PokemonList}/>
+                    <Route path="/pokemons/:id" component={PokemonDetail}/>
+                    <Route component={PageNotFound}/>
+                </Switch>
+            </div>
+        </Router>
     )
 }
 
--- src/components/pokemon-card.tsx
+++ src/components/pokemon-card.tsx
@@ -1,7 +1,9 @@
 import React, { FunctionComponent, useState } from "react";
 import Pokemon from "../models/pokemon";
-
 import "./pokemon-card.css";
+import formatDate from "../helpers/format-date";
+import formatType from "../helpers/format-type";
+import { useHistory } from "react-router-dom";
 
 type Props = {
     pokemon: Pokemon,
@@ -11,6 +13,7 @@ type Props = {
 const PokemonCard: FunctionComponent<Props> = ({pokemon, borderColor = "#009688"}) =>
 {
     const [color, setColor] = useState<string>();
+    const history = useHistory();
 
     const showBorder = () => {
         setColor(borderColor);
@@ -20,56 +23,12 @@ const PokemonCard: FunctionComponent<Props> = ({pokemon, borderColor = "#009688"
         setColor("#f5f5f5");
     }
 
-    const formatDate = (date: Date): string => {
-        return `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`;
-    }
-
-    const formatType = (type: string): string => {
-        let color: string;
-        switch (type) {
-            case "Feu":
-                color = "red lighten-1";
-                break;
-            case "Eau":
-                color = "blue lighten-1";
-                break;
-            case "Plante":
-                color = "green lighten-1";
-                break;
-            case "Insecte":
-                color = "brown lighten-1";
-                break;
-            case "Normal":
-                color = "grey lighten-3";
-                break;
-            case "Vol":
-                color = "blue lighten-3";
-                break;
-            case "Poison":
-                color = "deep-purple accent-1";
-                break;
-            case "Fée":
-                color = "pink lighten-4";
-                break;
-            case "Psy":
-                color = "deep-purple darken-2";
-                break;
-            case "Electrik":
-                color = "lime accent-1";
-                break;
-            case "Combat":
-                color = "deep-orange";
-                break;
-            default:
-                color = "grey";
-                break;
-        }
-
-        return `chip ${color}`;
+    const goToPokemon = (id: number) => {
+        history.push(`/pokemons/${id}`);
     }
 
     return (
-        <div className="col s6 m4" onMouseEnter={showBorder} onMouseLeave={hideBorder}>
+        <div className="col s6 m4" onClick={() => goToPokemon(pokemon.id)} onMouseEnter={showBorder} onMouseLeave={hideBorder}>
             <div className="card horizontal" style={{borderColor: color}}>
                 <div className="card-image">
                     <img src={pokemon.picture} alt={pokemon.name}/>
