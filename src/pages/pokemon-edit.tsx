983e11afbc6a3c2a2a281fc630cef26c154f7179
Robin Ponçon <poncon.robin@gmail.com>
2020-12-10 16:42:13 +0100

    Création d'une API avec json-server et création des requêtes à cette api

--- src/pages/pokemon-edit.tsx
+++ src/pages/pokemon-edit.tsx
@@ -2,7 +2,7 @@ import React, { FunctionComponent, useState, useEffect } from "react";
 import { RouteComponentProps } from "react-router-dom";
 import PokemonForm from "../components/pokemon-form";
 import Pokemon from "../models/pokemon";
-import {POKEMONS} from "../models/mock-pokemon";
+import PokemonService from "../services/pokemon-service";
 
 type Params = { id: string};
 
@@ -10,12 +10,7 @@ const PokemonEdit: FunctionComponent<RouteComponentProps<Params>> = ({ match}) =
     const [pokemon, setPokemon] = useState<Pokemon|null>(null);
 
     useEffect(() => {
-        POKEMONS.forEach(pokemon => {
-            if (match.params.id === pokemon.id.toString())
-            {
-                setPokemon(pokemon);
-            }
-        })
+        PokemonService.getPokemon(+match.params.id).then(pokemon => setPokemon(pokemon));
     }, [match.params.id]);
 
     return (
--- src/pages/pokemon-list.tsx
+++ src/pages/pokemon-list.tsx
@@ -1,13 +1,13 @@
 import React, { FunctionComponent, useState, useEffect } from "react";
 import Pokemon from "../models/pokemon";
-import {POKEMONS} from "../models/mock-pokemon";
 import PokemonCard from "../components/pokemon-card";
+import PokemonService from "../services/pokemon-service";
 
 const PokemonList: FunctionComponent = () => {
     const [pokemons, setPokemons] = useState<Pokemon[]>([]);
 
     useEffect(() => {
-       setPokemons(POKEMONS);
+       PokemonService.getPokemons().then(pokemons => setPokemons(pokemons));
     }, []);
 
     return(
