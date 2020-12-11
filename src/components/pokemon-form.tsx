630fdc2890077fd5909097d309ce379ae0bcbea3
Robin Ponçon <poncon.robin@gmail.com>
2020-12-11 16:29:48 +0100

    Persistance des données lors de l'édition d'un Pokémon + ajout d'un bouton de suppression

--- src/components/pokemon-form.tsx
+++ src/components/pokemon-form.tsx
@@ -2,6 +2,7 @@ import React, { FunctionComponent, useState} from "react";
 import { useHistory } from "react-router-dom";
 import Pokemon from "../models/pokemon";
 import formatType from "../helpers/format-type";
+import PokemonService from "../services/pokemon-service";
 
 type Props = {
     pokemon: Pokemon
@@ -131,8 +132,16 @@ const PokemonForm: FunctionComponent<Props> = ({pokemon}) => {
         const isFormValid = validateForm();
         if(isFormValid)
         {
-            history.push(`/pokemons/${pokemon.id}`);
+            pokemon.name = form.name.value;
+            pokemon.hp = form.hp.value;
+            pokemon.cp = form.cp.value;
+            pokemon.types = form.types.value;
+            PokemonService.updatePokemon(pokemon).then(() => history.push(`/pokemons/${pokemon.id}`));
         }
+    }
+
+    const deletePokemon= () => {
+        PokemonService.deletePokemon(pokemon).then(() => history.push(`/pokemons`));
     }
 
     return (
@@ -142,6 +151,9 @@ const PokemonForm: FunctionComponent<Props> = ({pokemon}) => {
                     <div className="card hoverable">
                         <div className="card-image">
                             <img src={pokemon.picture} alt={pokemon.name} style={{width: "250px", margin: "0 auto"}}/>
+                            <span className="btn-floating halfway-fab waves-effect waves-light">
+                                <i onClick={deletePokemon} className="material-icons">delete</i>
+                            </span>
                         </div>
                         <div className="card-stacked">
                             <div className="card-content">
