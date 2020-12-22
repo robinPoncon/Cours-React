2dc3e34799a10413a27a2da4ad657d84e534a3c1
Robin Ponçon <poncon.robin@gmail.com>
2020-12-22 16:47:41 +0100

    Ajout d'une fonctionnalité de recherche et authentification utilisateur

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import PageNotFound from "./pages/page-not-found";
 import PokemonEdit from "./pages/pokemon-edit";
 import PokemonAdd from "./pages/pokemon-add";
+import Login from "./pages/login";
 
 const App: FunctionComponent = () => {
 
@@ -20,6 +21,7 @@ const App: FunctionComponent = () => {
                 {/* Le système de gestion des routes de notre application */}
                 <Switch>
                     <Route exact path="/" component={PokemonList}/>
+                    <Route exact path="/login" component={Login}/>
                     <Route exact path="/pokemons" component={PokemonList}/>
                     <Route exact path="/pokemons/add" component={PokemonAdd}/>
                     <Route exact path="/pokemons/edit/:id" component={PokemonEdit}/>
--- src/pages/pokemon-list.tsx
+++ src/pages/pokemon-list.tsx
@@ -2,7 +2,8 @@ import React, { FunctionComponent, useState, useEffect } from "react";
 import Pokemon from "../models/pokemon";
 import PokemonCard from "../components/pokemon-card";
 import PokemonService from "../services/pokemon-service";
-import {RouteComponentProps, Link} from "react-router-dom";
+import {Link} from "react-router-dom";
+import PokemonSearch from "../components/pokemon-search";
 
 const PokemonList: FunctionComponent = () => {
     const [pokemons, setPokemons] = useState<Pokemon[]>([]);
@@ -16,6 +17,7 @@ const PokemonList: FunctionComponent = () => {
             <h1 className="center">Pokédex</h1>
             <div className="container">
                 <div className="row">
+                    <PokemonSearch/>
                     {pokemons.map(pokemon => (
                       <PokemonCard key={pokemon.id} pokemon={pokemon}/>
                     ))}
